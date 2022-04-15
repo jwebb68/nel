70226910183ff0994ee3f79501a86f1efa0459fb
Jason Webb <jwebb@tuffmail.co.uk>
2022-04-15 14:50:29 +0100

    memory: add documentation

--- src/nel/memory.hh
+++ src/nel/memory.hh
@@ -9,11 +9,34 @@
 namespace nel
 {
 
+/**
+ * copy memory [s,s+n) to [d,d+n)
+ * Caller is required to make sure there is enough space allocated in d
+ */
 void memcpy(uint8_t *const d, uint8_t const *const s, Length const n) noexcept;
+
+/**
+ * set memory [d,d+n) to value s
+ * Caller is required to make sure there is enough space allocated in d
+ */
 void memset(uint8_t *const d, uint8_t const s, Length const n) noexcept;
+
+/**
+ * move memory [s,s+n) to [d,d+n)
+ * Caller is required to make sure there is enough space allocated in d
+ * [s,s+n) is set to default debug pattern (0xa5)
+ */
 void memmove(uint8_t *const d, uint8_t *const s, Length const n) noexcept;
+
+/**
+ * bytewise swap memory [s,s+n) with [d,d+n)
+ */
 void memswap(uint8_t *const d, uint8_t *const s, Length const n) noexcept;
 
+/**
+ * move items [s,s+n) to [d,d+n)
+ * caller must ensure there is enough space in d.
+ */
 template<typename T>
 void memmove(T *d, T *s, Length n) noexcept
 {
@@ -22,6 +45,10 @@ void memmove(T *d, T *s, Length n) noexcept
     }
 }
 
+/**
+ * copy items [s,s+n) to [d,d+n)
+ * caller must ensure there is enough space in d.
+ */
 template<typename T>
 void memcpy(T *const d, T const *const s, Length n) noexcept
 {
@@ -30,6 +57,11 @@ void memcpy(T *const d, T const *const s, Length n) noexcept
     }
 }
 
+
+/**
+ * set items [s,s+n) to (copy of) d
+ * caller must ensure there is enough space in d.
+ */
 template<typename T>
 void memset(T *const d, T const &s, Length n) noexcept
 {
@@ -38,6 +70,9 @@ void memset(T *const d, T const &s, Length n) noexcept
     }
 }
 
+/**
+ * swap items [s,s+n) with [d,d+n)
+ */
 template<typename T>
 void memswap(T *const d, T *const s, Length n) noexcept
 {
