b1aec9de32dd12ef1cba866cab2b9af1ce36221d
Jason Webb <jwebb@tuffmail.co.uk>
2023-09-03 08:29:01 +0100

    resync from arduino-tesla project

---
+++ src/nel/phantom.hh
@@ -0,0 +1,16 @@
+// -*- mode: c++; indent-tabs-mode: nil; tab-width: 4 -*-
+#if !defined(NEL_PHANTOM_HH)
+#    define NEL_PHANTOM_HH
+
+namespace nel
+{
+
+template<typename T>
+struct Phantom
+{
+        constexpr Phantom() = default;
+};
+
+}; // namespace nel
+
+#endif //! defined(NEL_PHANTOM_HH)
---
+++ src/nel/time/duration.cc
@@ -0,0 +1,2 @@
+// -*- mode: c++; indent-tabs-mode: nil; tab-width: 4 -*-
+#include <nel/time/duration.hh>
